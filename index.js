c46b8ee28c4fc25137f32c3ca9e5943af35cf0f3
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-04 18:01:15 -0500

    commit tree 2b3c1dee3f9f956918754a19d30641ee7a3e0eb6

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+// function findMatching(drives) {
+//     for (let name of drives) {
+//         if( strin)
+//     }
+// }
+
+// function UpperCase(drivers) {
+//     return drives[name]
+// }
+
+
+
+
+// const findMatching = drivers.filter(drive => driver === n
+
+
+  
+
+
+function findMatching(array, name) { 
+     return array.filter(drivers => drivers.toLowerCase() === name.toLowerCase()) 
+     
+}
+// console.log(findMatching(driver));
+
+// const fuzzyMatch = drivers.filter((name) => name.startsWith(""));
+
+function fuzzyMatch(array,name) {
+    return array.filter(drivers => drivers.startsWith(name));
+}
+
+function matchName (array,name, hometown) {
+    return array.filter(drivers => (drivers.name = name) ? name : hometown)
+}
+
+
